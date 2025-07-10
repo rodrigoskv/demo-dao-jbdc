44c1010ae1496ccae783ea4b60e3a6d222430c06
rodrigoskv <166267162+rodrigoskv@users.noreply.github.com>
2025-07-10 14:47:17 -0300

    Update DepartmentDao.java

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,18 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteByID(Integer id);
+	Department findId(Integer id);
+	List<Department> findAll();
+	
+	
+	
+		
 }
